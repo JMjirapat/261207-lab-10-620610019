d29df66d74fa39fbc508ff7ed494861e093528b0
JMjirapat <jm_jirapat@hotmail.com>
2022-09-24 16:49:21 +0700

    finish

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -1,16 +1,50 @@
 import { readDB, writeDB } from "../../../../backendLibs/dbLib";
 import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/router";
 
 export default function roomIdMessageRoute(req, res) {
-  if (req.method === "GET") {
-    const rooms = readDB();
-  } else if (req.method === "POST") {
-    const rooms = readDB();
-
-    //read request body
-    const text = req.body.text;
-
-    //create new id
-    const newId = uuidv4();
-  }
+	if (req.method === "GET") {
+		const rooms = readDB();
+		const roomId = req.query.roomId;
+		const data = rooms.filter((element) => {
+			return element.roomId == roomId;
+		});
+		if (data[0]) {
+			return res.status(200).json({
+				ok: true,
+				messages: data[0].messages,
+			});
+		} else {
+			return res.status(404).json({
+				ok: false,
+				message: "Invalid room id",
+			});
+		}
+	} else if (req.method === "POST") {
+		//create new id
+		const newId = uuidv4();
+		const rooms = readDB();
+		const roomId = req.query.roomId;
+		const text = req.body.text;
+		const data = rooms.filter((element) => {
+			return element.roomId == roomId;
+		});
+		if (data[0]) {
+			if (typeof text !== "string" || text.length < 1) {
+				return res
+					.status(400)
+					.json({ ok: false, message: "Invalid text input" });
+			} else {
+				const newpost = { messageId: newId, text: text };
+				data[0].messages = [...data[0].messages, newpost];
+				writeDB(data);
+				res.status(201).json({ ok: true, message: newpost });
+			}
+		} else {
+			return res.status(404).json({
+				ok: false,
+				message: "Invalid room id",
+			});
+		}
+	}
 }
